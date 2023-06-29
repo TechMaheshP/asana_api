fa68111b5159458d22f45126664d1f59cf8e3898
techmahesh86 <techmahesh86@gmail.com>
2023-06-29 19:17:00 +0530

    add comments for specific methods

--- asana_api.rb
+++ asana_api.rb
@@ -9,42 +9,55 @@ class AsanaApi
   TOKEN = '1/1204892460316154:9d9c0f18d242721303ed8511113d5e11'
 
   class << self
+    # Method to create project which have require following params
+    # data: Hash, team: uniq team ID, current_status: Hash of reuired project params like title, text and etc. 
     def create_project(params)
       url = get_url('projects')
 
       get_response(url, post_request(url), params)
     end
 
+    # Method to create section for any project which have require following params
+    # project_gid: project ID (mandatory), name: section name(mandatory).
     def create_section(project_gid, params)
       url = get_url("projects/#{project_gid}/sections")
 
       get_response(url, post_request(url), params)
     end
 
+    # Method to find section any section using section_gid
+    # section_gid: Section ID (mandatory).
     def get_section(section_gid)
       url = get_url("sections/#{section_gid}")
 
       get_response(url, get_request(url))
     end
 
+    # Method to get all workspaces.
     def get_workspaces
       url = get_url('workspaces')
 
       get_response(url, get_request(url))
     end
 
+    # Method to create task for any workspace which have require following params
+    # workspace: project ID (mandatory), name: task name and any other params.
     def create_task(params)
       url = get_url('tasks')
 
       get_response(url, post_request(url), params)
     end
 
+    # Method to update task recently created task which have require following params
+    # workspace: project ID (mandatory), name: task name and any other params.
     def update_task(task_gid, params)
       url = get_url("tasks/#{task_gid}")
 
       request = Net::HTTP::Put.new(url)
       get_response(url, request, params)
     end
+
+    protected
 
     def get_url(end_point)
       URI("#{API_ENDPOINT}#{end_point}")
@@ -72,3 +85,5 @@ class AsanaApi
     end
   end
 end
+
+p AsanaApi.get_section(1204894009803251)
